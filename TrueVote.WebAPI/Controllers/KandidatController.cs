15f28b3c8c5e7282e6ff49265610406f9867bf2a
Sanjin Mekic <sanjin.mekic@edu.fit.ba>
2026-01-04 14:08:05 +0100

    Locking Kandidat enpoints

--- TrueVote.WebAPI/Controllers/KandidatController.cs
+++ TrueVote.WebAPI/Controllers/KandidatController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TrueVote.Model.Requests;
 using TrueVote.Model.Responses;
 using TrueVote.Model.SearchObjects;
@@ -6,6 +7,7 @@ using TrueVote.Services;
 
 namespace TrueVote.WebAPI.Controllers
 {
+    [Authorize]
     public class KandidatController : BaseCRUDController<KandidatResponse, KandidatSearchObject, KandidatInsertRequest, KandidatUpdateRequest>
     {
         IKandidatService _service;
@@ -18,6 +20,24 @@ namespace TrueVote.WebAPI.Controllers
         public ActionResult<bool> CanDelete(int id)
         {
             return Ok(new { canDelete = _service.CanDelete(id) });
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override KandidatResponse Insert(KandidatInsertRequest request)
+        {
+            return base.Insert(request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override KandidatResponse Update(int id, KandidatUpdateRequest request)
+        {
+            return base.Update(id, request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override void Delete(int id)
+        {
+            base.Delete(id);
         }
     }
 }
