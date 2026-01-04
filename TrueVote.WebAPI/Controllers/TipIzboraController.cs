f8417c9f30c9dd338d03338119f889b59dacb00c
Sanjin Mekic <sanjin.mekic@edu.fit.ba>
2026-01-04 14:14:25 +0100

    Locking TipIzbora endpoints

--- TrueVote.WebAPI/Controllers/TipIzboraController.cs
+++ TrueVote.WebAPI/Controllers/TipIzboraController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TrueVote.Model.Requests;
 using TrueVote.Model.Responses;
 using TrueVote.Model.SearchObjects;
@@ -6,6 +7,7 @@ using TrueVote.Services;
 
 namespace TrueVote.WebAPI.Controllers
 {
+    [Authorize]
     public class TipIzboraController : BaseCRUDController<TipIzboraResponse, TipIzboraSearchObject, TipIzboraInsertRequest, TipIzboraUpdateRequest>
     {
         ITipIzboraService _service;
@@ -14,10 +16,29 @@ namespace TrueVote.WebAPI.Controllers
             _service = service;
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpGet("{id}/can-delete")]
         public ActionResult<bool> CanDelete(int id)
         {
             return Ok(new { canDelete = _service.CanDelete(id) });
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override TipIzboraResponse Insert(TipIzboraInsertRequest request)
+        {
+            return base.Insert(request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override TipIzboraResponse Update(int id, TipIzboraUpdateRequest request)
+        {
+            return base.Update(id, request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override void Delete(int id)
+        {
+            base.Delete(id);
         }
     }
 }
