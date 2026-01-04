0851e97e319ff9ad4b695f839c3e1603c2a4d8d9
Sanjin Mekic <sanjin.mekic@edu.fit.ba>
2026-01-04 14:12:53 +0100

    Locking Pitanje endpoints

--- TrueVote.WebAPI/Controllers/PitanjeController.cs
+++ TrueVote.WebAPI/Controllers/PitanjeController.cs
@@ -6,11 +6,30 @@ using TrueVote.Services;
 
 namespace TrueVote.WebAPI.Controllers
 {
+    [Authorize]
     public class PitanjeController : BaseCRUDController<PitanjeResponse, PitanjeSearchObject, PitanjeInsertRequest, PitanjeUpdateRequest>
     {
         IPitanjeService _service;
         public PitanjeController(IPitanjeService service) : base(service)
         {
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override PitanjeResponse Insert(PitanjeInsertRequest request)
+        {
+            return base.Insert(request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override PitanjeResponse Update(int id, PitanjeUpdateRequest request)
+        {
+            return base.Update(id, request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override void Delete(int id)
+        {
+            base.Delete(id);
         }
     }
 }
