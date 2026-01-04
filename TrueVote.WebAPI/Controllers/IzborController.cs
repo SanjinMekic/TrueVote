629a92e0a98ab5bf58eef227dade575a3a4adaf3
Sanjin Mekic <sanjin.mekic@edu.fit.ba>
2026-01-04 14:07:18 +0100

    Locking Izbor endpoints

--- TrueVote.WebAPI/Controllers/IzborController.cs
+++ TrueVote.WebAPI/Controllers/IzborController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TrueVote.Model.Requests;
 using TrueVote.Model.Responses;
 using TrueVote.Model.SearchObjects;
@@ -6,6 +7,7 @@ using TrueVote.Services;
 
 namespace TrueVote.WebAPI.Controllers
 {
+    [Authorize]
     public class IzborController : BaseCRUDController<IzborResponse, IzborSearchObject, IzborInsetRequest, IzborUpdateRequest>
     {
         IIzborService _service;
@@ -14,6 +16,7 @@ namespace TrueVote.WebAPI.Controllers
             _service = service;
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpGet("{id}/can-delete")]
         public ActionResult<bool> CanDelete(int id)
         {
@@ -27,11 +30,30 @@ namespace TrueVote.WebAPI.Controllers
             return Ok(result);
         }
 
+        [Authorize(Roles = "Birac")]
         [HttpGet("aktivni-izbori/korisnik/{korisnikId}")]
         public async Task<IActionResult> GetAktivniIzboriZaKorisnika(int korisnikId)
         {
             var result = await _service.GetAktivniIzboriZaKorisnikaAsync(korisnikId);
             return Ok(result);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override IzborResponse Insert(IzborInsetRequest request)
+        {
+            return base.Insert(request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override IzborResponse Update(int id, IzborUpdateRequest request)
+        {
+            return base.Update(id, request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override void Delete(int id)
+        {
+            base.Delete(id);
         }
     }
 }
