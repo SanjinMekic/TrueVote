f9b6b694b70edd9d4dc9c70d4fd556667274579b
Sanjin Mekic <sanjin.mekic@edu.fit.ba>
2026-01-04 14:09:27 +0100

    Locking Kategorija endpoints

--- TrueVote.WebAPI/Controllers/KategorijaController.cs
+++ TrueVote.WebAPI/Controllers/KategorijaController.cs
@@ -1,16 +1,36 @@
-﻿using TrueVote.Model.Requests;
+﻿using Microsoft.AspNetCore.Authorization;
+using TrueVote.Model.Requests;
 using TrueVote.Model.Responses;
 using TrueVote.Model.SearchObjects;
 using TrueVote.Services;
 
 namespace TrueVote.WebAPI.Controllers
 {
+    [Authorize]
     public class KategorijaController : BaseCRUDController<KategorijaResponse, KategorijaSearchObject, KategorijaInsertRequest, KategorijaUpdateRequest>
     {
         IKategorijaService _service;
         public KategorijaController(IKategorijaService service) : base(service) 
         {
             _service = service;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override KategorijaResponse Insert(KategorijaInsertRequest request)
+        {
+            return base.Insert(request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override KategorijaResponse Update(int id, KategorijaUpdateRequest request)
+        {
+            return base.Update(id, request);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public override void Delete(int id)
+        {
+            base.Delete(id);
         }
     }
 }
